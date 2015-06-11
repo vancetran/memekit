ecb52af98aef3ea8ef4d5af1f8c5bded7434d377
Vance Tran <v@vancetran.com>
2015-06-11 14:50:02 -0700

    Some HTML and CSS scaffolding

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,12 +4,4 @@ var memeApp = angular.module('memeApp', [
 
 memeApp.config(['$routeProvider', function($routeProvider) {
 
-  var s = Snap("#svg");
-  var bigCircle = s.circle(150, 150, 100);
-  bigCircle.attr({
-    fill: "#bada55",
-    stroke: "#000",
-    strokeWidth: 5
-  });
-  
 }]);
