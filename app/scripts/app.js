64a4586452d2425c92045f3994745d270c1d77e4
Vance Tran <v@vancetran.com>
2015-06-11 16:25:26 -0700

    Angular basics

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,25 @@
-var memeApp = angular.module('memeApp', [
-  'ngRoute'
-]);
+'use strict';
+/**
+ * @ngdoc overview
+ * @name memekitApp
+ * @description
+ * # memekitApp
+ *
+ * Main module of the application.
+ */
 
-memeApp.config(['$routeProvider', function($routeProvider) {
+(function(){
 
-}]);
+  var app = angular.module('memekit', []);
+
+  app.controller('MemekitController', function() {
+    this.slate = meme;
+  });
+
+  var meme = {
+    caption: 'Hello world.',
+    source: 'Vance',
+    target: 'twitter'
+  };
+
+})();
