dbce3d1d90ca628c5bb60bd8dfcaf654512cbc64
Vance Tran <v@vancetran.com>
2015-06-16 16:46:53 -0700

    reset and draw

--- app/scripts/directives/snapsvg.js
+++ app/scripts/directives/snapsvg.js
@@ -17,9 +17,7 @@ angular.module('memekit')
         svgTheme: '=',
       },
       link: function postLink(scope, element){
-        console.log("console loggin' scope.config");
-        // console.log(scope);
-
+        console.log("console loggin' scope.svgConfig");
         console.log(scope.svgConfig);
 
         var s = snapSVG(element[0].children[0]);
@@ -28,15 +26,30 @@ angular.module('memekit')
           width: '100%',
         });
 
-        console.log("log element: ");
+        console.log('log element: ');
         console.log(element);
 
-        var x = s.text(20, 40, "Hello SVG");
+        var x = s.text(20, 40, 'Hello SVG');
         x.drag();
+        x.attr({fill: '#f00'});
 
         var elementData = element;
 	      delete elementData.$$hashKey;
 
+        var drawElements = function () {
+          console.log('pretending to draw elements (stub)');
+        };
+
+        var resetSvg = function () {
+          console.log("resetSvg run");
+          var els = s.selectAll('*');
+          angular.forEach(els, function(e, i) {
+            e.transform('');
+          });
+        }
+
+        scope.$watch('svgConfig', drawElements, true);
+        scope.$on('resetSvg', resetSvg);
       }
     };
 });
