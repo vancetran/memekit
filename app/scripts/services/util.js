d521817c36a6ace4b6bc1d569b7b7f1a5993e522
Vance Tran <v@vancetran.com>
2015-08-27 11:29:05 -0700

    onbeforeunload verbiage update

--- app/scripts/services/util.js
+++ app/scripts/services/util.js
@@ -17,7 +17,7 @@ var app = angular.module('memekit')
       // If we haven't been passed the event get the window.event
       e = e || window.event;
 
-      var message = 'Warning: If you leave or reload this page, your changes will be lost.';
+      var message = 'Unsaved changes will be lost if you leave or reload the page.';
 
       // For IE6-8 and Firefox prior to version 4
       if (e)
